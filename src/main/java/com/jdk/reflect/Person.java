887d6c35d076b266dbae31c6af085913e3562106
Dxh7788 <anan1215>
2018-08-06 13:14:03 +0800

    泛型 类型变量

--- src/main/java/com/jdk/reflect/Person.java
+++ src/main/java/com/jdk/reflect/Person.java
@@ -1,14 +1,10 @@
 package com.jdk.reflect;
 
-
 /**
  * Copyright (C) 2017-2018 https://www.htouhui.com - A project by JDKSrcAna
  *
  * @author xh.d
- * @since 2018/8/6 12:12
+ * @since 2018/8/6 13:04
  */
-public class Common<T> {
-    public User<?> getCommon(){
-        return new User();
-    }
+public class Person<T> {
 }
---
+++ src/main/java/com/jdk/reflect/User.java
@@ -0,0 +1,10 @@
+package com.jdk.reflect;
+
+/**
+ * @author xh.d
+ * @since 2018/8/6 13:03
+ */
+public class User<T> extends Person<User> {
+
+
+}
